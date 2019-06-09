000a0696f50f5e687a2a7502ac914d9fc4b5a09b
MarkGelman <47790935+MarkGelman@users.noreply.github.com>
2019-06-09 14:21:47 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -12,7 +12,9 @@ namespace Star_Triangle
         {
             int stars = 1;
             int space = 0;
-            bool flag = true;
+            bool flag = true;//The sign indicates that the programm has completed half the work.
+                             //From this point, the number of stars in the line starts to decrease
+                             // (sign = true (increase), sign = "false" (decrease)
             Console.WriteLine("Enter odd number of lines:");
             int lines = int.Parse(Console.ReadLine());
             for (int i=1; i<= lines;i++)
